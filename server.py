770fa663a1dbd9fb2cb5f71c4bce2f66187a8040
unknown <honglingbupt@gnail.com>
2019-04-27 06:33:26 -0700

    add getRequest

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ import chaosmonkey_pb2
 import chaosmonkey_pb2_grpc
 from utils import load_config
 from chaos_server import ChaosServer
+from logging import Logger, StreamHandler, Formatter
+
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -19,7 +21,11 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
         self.configs = load_config(config_path)
         self.myIp = myIp
         self.myPort = myPort
+        self.leaderId = ""
+        self.serverId = ""
         self.storage = {}
+        self.term = 0
+        self.logger = self.set_log()
 
         self.node_index = 0
         for t in self.configs['nodes']:
@@ -28,10 +34,26 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
             self.node_index += 1
 
     def Get(self, request, context):
-        if request.key in self.storage:
-            return storage_service_pb2.GetResponse(value=str(self.storage[request.key]), ret=0)
+        if self.is_leader():
+            return storage_service_pb2.GetResponse(leaderId=self.leaderId, ret=1)
+        self.heartbeat_once()
+        if self.is_leader() and request.key in self.storage:
+            return storage_service_pb2.GetResponse(value=str(self.storage[request.key]), leaderId="", ret=0)
         else:
             return storage_service_pb2.GetResponse(ret=1)
+
+    def heartbeat_once(self, node):
+        self.logger.info("sending heartbeat to {}".format(node))
+        try:
+            term, success = self.conns[node].root.append_entries(self.term, self.serverId)
+            if term > self.term:
+                self.convert_to_follower(term)
+        except Exception as e:
+            self.logger.error(e)
+
+    def heartbeat_once_to_all(self):
+
+
 
     def Put(self, request, context):
         global conn_mat
@@ -43,6 +65,18 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
         self.storage[request.key] = request.value
         self.broadcast_to_all_nodes(request)
         return storage_service_pb2.PutResponse(ret=0)
+
+    def is_leader(self):
+        return self.leaderId == self.serverId
+
+    def set_log(self):
+        logger = Logger(self.serverId)
+        # logger.addHandler(FileHandler("{}_{}.log".format(PERSISTENT_PATH_PREFIC, self.name)))
+        ch = StreamHandler()
+        ch.setFormatter(Formatter("%(asctime)s %(levelname)s %(message)s"))
+        logger.addHandler(ch)
+        logger.setLevel("INFO")
+        return logger
 
 
 class ChaosServer(chaosmonkey_pb2_grpc.ChaosMonkeyServicer):
