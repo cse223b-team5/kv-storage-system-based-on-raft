088b8ae75422cf8849c453b31390ab51e6eaf9f7
Jie Lu <j4lu@ucsd.edu>
2019-04-27 02:31:58 -0700

    Put

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import time
 import sys
 import logging
 import random
+import threading
+import functools
 import grpc
 import storage_service_pb2
 import storage_service_pb2_grpc
@@ -10,6 +12,24 @@ import chaosmonkey_pb2
 import chaosmonkey_pb2_grpc
 from utils import load_config
 from chaos_server import ChaosServer
+
+lock_append_log = threading.Lock()
+lock_ae_succeed_cnt = threading.Lock()
+lock_decrement_nextIndex = threading.Lock()
+lock_try_extend_nextIndex = threading.Lock()
+lock_try_extend_matchIndex = threading.Lock()
+
+
+def synchronized(lock):
+    """ Synchronization decorator """
+    def wrap(f):
+        @functools.wraps(f)
+        def new_function(*args, **kw):
+            with lock:
+                return f(*args, **kw)
+        return new_function
+    return wrap
+
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -20,12 +40,29 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
         self.myIp = myIp
         self.myPort = myPort
         self.storage = {}
+        self.node_index = self.get_node_index_by_addr(myIp, myPort)
 
-        self.node_index = 0
+        self.currentTerm = 0
+        self.voteFor = 0
+        self.log = list()  # [(k,v)] list of tuples
+        self.log_term = list()  # []
+        self.commitIndex = 0 # has been committed
+        self.lastApplied = 0
+        self.nextIndex = list()  # has not yet been appended
+        self.matchIndex = list()  # has been matched
+        # TODO: call a function to properly initialize these variables
+
+        self.leaderIndex = 0
+        self.last_commit_history = dict() # client_id -> (serial_no, result)
+        self.ae_succeed_cnt = 0
+
+    def get_node_index_by_addr(self, ip, port):
+        index = 0
         for t in self.configs['nodes']:
-            if t[0] == myIp and t[1] == myPort:
+            if t[0] == ip and t[1] == port:
                 break
-            self.node_index += 1
+            index += 1
+        return index
 
     def Get(self, request, context):
         if request.key in self.storage:
@@ -33,15 +70,131 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
         else:
             return storage_service_pb2.GetResponse(ret=1)
 
+    def check_is_leader(self):
+        return self.node_index == self.leaderIndex
+
+    @synchronized(lock_append_log)
+    def append_to_local_log(self, key, value):
+        self.log.append((key, value))
+
+    @synchronized(lock_ae_succeed_cnt)
+    def increment_ae_succeed_cnt(self):
+        self.ae_succeed_cnt += 1
+
+    @synchronized(lock_decrement_nextIndex)
+    def decrement_nextIndex(self, node_index):
+        self.node_index[node_index] -= 1
+
+    @synchronized(lock_try_extend_nextIndex)
+    def try_extend_nextIndex(self, node_index, new_nextIndex):
+        if new_nextIndex > self.nextIndex[node_index]:
+            self.nextIndex[node_index] = new_nextIndex
+
+    @synchronized(lock_try_extend_matchIndex)
+    def try_extend_matchIndex(self, node_index, new_matchIndex):
+        if new_matchIndex > self.matchIndex[node_index]:
+            self.matchIndex[node_index] = new_matchIndex
+
+    def generate_request_to(self, ip, port):
+        request = storage_service_pb2.AppendEntriesRequest()
+        request.term = self.currentTerm
+        request.leaderId = self.node_index
+
+        node_index = self.get_node_index_by_addr(ip, port)
+        entry_start_index = self.nextIndex[node_index]
+        request.prevLogIndex = entry_start_index - 1
+        request.prevLogTerm = self.log_term[request.prevLogIndex]
+        es = self.log[entry_start_index:]
+        for e in es:
+            entry = request.entries.add()
+            entry.key = str(e[0])
+            entry.value = str(e[1])
+
+        request.leaderCommit = self.commitIndex
+
+        new_nextIndex = entry_start_index + len(es)
+        return request, new_nextIndex
+
+    def update_nextIndex_and_matchIndex(self, node_index, new_nextIndex):
+        # since you can't tell if the 'success' reply from AE is covered by any later AE, you need to
+        # manually pass the new_nextIndex as arg to this function
+        self.try_extend_nextIndex(node_index, new_nextIndex)
+        # same case for matchIndex
+        self.try_extend_matchIndex(node_index, new_nextIndex - 1)
+
+    def replicate_log_entries_to_one(self, ip, port):
+        with grpc.insecure_channel(ip + ':' + port) as channel:
+            stub = storage_service_pb2_grpc.KeyValueStoreStub(channel)
+            request, new_nextIndex = self.generate_request_to(ip, port)
+            try:
+                response = stub.AppendEntries(request, timeout=1)
+            except TimeoutError:
+                # TODO: once timeout we suppose it has failed?
+                return
+
+            if response.success:
+                self.increment_ae_succeed_cnt()
+                receiver_index = self.get_node_index_by_addr(ip,port)
+                self.update_nextIndex_and_matchIndex(receiver_index, new_nextIndex)
+            elif response.failed_for_term:
+                # TODO: step down to follower
+                pass
+            else:
+                # AppendEntries failed because of log inconsistency
+                receiver_index = self.get_node_index_by_addr(ip, port)
+                self.decrement_nextIndex(receiver_index)
+                self.replicate_log_entries_to_one(ip, port)
+
+    def replicate_log_entries_to_all(self):
+        for ip, port in self.configs['node']:
+            ae_thread = threading.Thread(target=self.replicate_log_entries_to_one, args=(ip, port))
+            ae_thread.start()
+
     def Put(self, request, context):
-        global conn_mat
-        try:
-            conn_mat
-            print('ConnMax exists')
-        except Exception as e:
-            print('ConnMax does not exist')
-        self.storage[request.key] = request.value
-        self.broadcast_to_all_nodes(request)
+        #  check if current node is leader. if not, help client redirect
+        if not self.check_is_leader():
+            ip = self.configs['node'][self.leaderIndex][0]
+            port = self.configs['node'][self.leaderIndex][1]
+            return storage_service_pb2.PutResponse(leader_ip=ip, leader_port=port)
+
+        # to guarantee the 'at-most-once' rule; check commit history
+        client_id = str(context.peer())
+        if client_id in self.last_commit_history:
+            if request.serial_no == self.last_commit_history[client_id][0] and self.last_commit_history[client_id][1]:
+                return storage_service_pb2.PutResponse(ret=0)
+
+        self.append_to_local_log(request.key, request.value)
+
+        self.replicate_log_entries_to_all()
+
+        majority_cnt = len(self.configs['node']) // 2 + 1
+        while self.ae_succeed_cnt < majority_cnt:
+            if not self.check_is_leader():
+                return storage_service_pb2.PutResponse(ret=1)
+                # TODO: is redirecting to actual leader necessary?
+            continue
+
+        if self.ae_succeed_cnt < majority_cnt:
+            # client request failed
+            return storage_service_pb2.PutResponse(ret=1)
+
+        # update commitIndex
+        i = self.commitIndex + 1
+        while True:
+            cnt = self.matchIndex.count(i)
+            if cnt < majority_cnt:
+                break
+            i += 1
+        i -= 1  # i is the max number to have appeared for more than majority_cnt times
+        if self.log[i] == self.currentTerm and self.check_is_leader():
+            self.commitIndex = i
+
+        # apply to state machine
+
+        # record in history
+        self.last_commit_history[client_id] = (request.serial_no, True)
+
+        # respond to client
         return storage_service_pb2.PutResponse(ret=0)
 
 
