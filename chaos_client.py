32f6c8eabdd7263486d50595bb6ecc9cdf7c31ed
Jie Lu <lujie1996@hotmail.com>
2019-05-01 17:53:59 -0700

    modify chaosclient

--- chaos_client.py
+++ chaos_client.py
@@ -42,20 +42,19 @@ class ChaosMonkey():
     def get_current_connMatrix(self):
         # return if_succeed, matrix
         #   if_succeed: 0 for succeeded, 1 for failed
-        for ip, port in self.configs['nodes']:
-            try:
-                with grpc.insecure_channel(ip + ':' + port) as channel:
-                    stub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
-                    response = stub.GetMatrix(chaosmonkey_pb2.Empty())
-                    matrix = list()
-                    for row in response.rows:
-                        to_row = list()
-                        for e in row.vals:
-                            to_row.append(e)
-                        matrix.append(to_row)
-                    return 0, matrix
-            except Exception as e:
-                return 1, list()
+        try:
+            with grpc.insecure_channel(ip + ':' + port) as channel:
+                stub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
+                response = stub.GetMatrix(chaosmonkey_pb2.Empty())
+                matrix = list()
+                for row in response.rows:
+                    to_row = list()
+                    for e in row.vals:
+                        to_row.append(e)
+                    matrix.append(to_row)
+                return 0, matrix
+        except Exception as e:
+            return 1, list()
 
     def kill_a_node(self, node_id):
         # return if_succeed
