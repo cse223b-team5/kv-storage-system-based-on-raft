0e4c7b3366ad4884b39c7ceddd9c5076c521f43c
EC2 Default User <ec2-user@ip-172-31-16-120.us-west-2.compute.internal>
2019-05-03 02:59:39 +0000

    debug server

--- server.py
+++ server.py
@@ -338,7 +338,7 @@ class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
                 self.logger.error('Node #{} When heartbeat to node{}, inconsistency detected.'.
                                   format(self.node_index, node_index))
                 self.decrement_nextIndex(node_index)
-                self.heartbeat_once_to_one(ip, port, node_index)
+                self.heartbeat_once_to_one(ip, port, node_index, hb_success_error_cnt, hb_success_error_lock)
 
     def heartbeat_once_to_all(self, hb_success_error_cnt=list(), hb_success_error_lock=None, is_sync_entry=True):
         self.logger.info('Start sending heartbeat_once_to_all.')
